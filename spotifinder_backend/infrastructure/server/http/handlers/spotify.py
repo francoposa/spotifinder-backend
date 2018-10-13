22577bc6bfba5b1af17110511be0384020bb387d
Franco Posa <f2j21p94@gmail.com>
2018-10-13 10:49:10 -0500

    now returning error codes

--- spotifinder_backend/infrastructure/server/http/handlers/spotify.py
+++ spotifinder_backend/infrastructure/server/http/handlers/spotify.py
@@ -10,12 +10,12 @@ RECOMMEND_URL = 'https://api.spotify.com/v1/recommendations'
 LIMIT = 'limit'
 
 async def get_resource_analysis(request: aiohttp.web.Request) -> aiohttp.web.Response:
-    auth_response = await _do_post_with_auth(AUTH_URL, request.app[SPOTIFY_CLIENT_ID], request.app[SPOTIFY_CLIENT_SECRET], {'grant_type': 'client_credentials'})
+    _, auth_response = await _do_post_with_auth(AUTH_URL, request.app[SPOTIFY_CLIENT_ID], request.app[SPOTIFY_CLIENT_SECRET], {'grant_type': 'client_credentials'})
     auth_token = auth_response["access_token"]
     query_params = request.query
-    analyze_response = await _do_get_with_resource_id(ANALYZE_URL, auth_token, query_params["spotify_uri"])
+    response_status, analyze_response = await _do_get_with_resource_id(ANALYZE_URL, auth_token, query_params["spotify_uri"])
     print(analyze_response)
-    return aiohttp.web.json_response({"analysis":analyze_response})
+    return aiohttp.web.json_response(analyze_response, status=response_status)
 
 
 async def _do_get_with_query_string(base_url, token, **kwargs):
@@ -27,7 +27,7 @@ async def _do_get_with_query_string(base_url, token, **kwargs):
         async with aiohttp.ClientSession() as session:
             async with session.get(base_url + query_string, headers=headers) as resp:
                 print("-> Response: {0}".format(resp.status))
-                return await resp.json()
+                return  resp.status, await resp.json()
     except Exception as e:
         print("ERR: {}".format(e))
 
@@ -39,7 +39,7 @@ async def _do_get_with_resource_id(base_url, token, resource_id):
         async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=headers) as resp:
                 print("-> Response: {0}".format(resp.status))
-                return await resp.json()
+                return resp.status, await resp.json()
     except Exception as e:
         print("ERR: {}".format(e))
 
@@ -48,6 +48,6 @@ async def _do_post_with_auth(url: str, client_id: str, client_secret:str, payloa
         async with aiohttp.ClientSession() as session:
             async with session.post(url, auth=aiohttp.BasicAuth(client_id, client_secret), data=payload) as resp:
                 print("-> Response: {0}".format(resp.status))
-                return await resp.json()
+                return resp.status, await resp.json()
     except Exception as e:
         print("ERR: {}".format(e))
